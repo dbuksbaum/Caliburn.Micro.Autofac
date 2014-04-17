71a01b78f898e8149735ee4c0c67f8db80a2d570
David Buksbaum <david.buksbaum@gmail.com>
2014-04-17 19:44:42 -0400

    Began work on PCL version
    Need to wait for AutoFac to go PCL

---
+++ src/Caliburn.Micro.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitleAttribute("Caliburn.Micro.Autofac")]
+[assembly: AssemblyDescriptionAttribute("Autofac Bootstrapper for Caliburn.Micro (PCL)")]
+[assembly: AssemblyProductAttribute("Caliburn.Micro.Autofac")]
+[assembly: AssemblyCompanyAttribute("David Buksbaum - http://buksbaum.us")]
+[assembly: AssemblyCopyrightAttribute("Copyright © David Buksbaum 2009-2014")]
+
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDelaySignAttribute(false)]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
